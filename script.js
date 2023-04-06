cb34545eb77977cb245ea78b28a499c1c430c1e8
rajuponnamanda <rajuponnamanda3239@gmail.com>
2023-04-06 18:23:28 +0530

    added js problem

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('bad world')
+let employee = [
+  {
+    id: 1,
+    name: "shivani",
+    salary: 25000,
+  },
+  {
+    id: 2,
+    name: "Def",
+    salary: 15000,
+  },
+  {
+    id: 3,
+    name: "Abc",
+    salary: 65000,
+  },
+  {
+    id: 4,
+    name: "Abc",
+    salary: 75000,
+  },
+];
+let ab = employee.filter((employee) => employee.salary >= 25000);
+console.log(
+  ab.map((employee) => employee.salary).reduce((prev, next) => prev + next)
+);
+console.log(ab);
